c6ee2deed51835a6c9f6c4a8a7ae01e20676f566
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-03-24 21:59:42 -0300

    Aula: Uma arquitetura para deploy

---
+++ source/Core/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Source\Core;
+
+/**
+ * FSPHP | Class Controller
+ *
+ * @author Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
+ * @package Source\Core
+ */
+class Controller
+{
+
+}
