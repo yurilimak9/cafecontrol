57af23659e720e74a12366255a279be76e601f00
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-03-26 00:17:09 -0300

    Aula: Blog, home, paginação e artigo

--- index.php
+++ index.php
@@ -23,6 +23,9 @@ $route->get("/", "Web:home");
 $route->get("/sobre", "Web:about");
 $route->get("/termos", "Web:terms");
 
+$route->get("/blog", "Web:blog");
+$route->get("/blog/page/{page}", "Web:blog");
+$route->get("/blog/{postName}", "Web:blogPost");
 
 /**
  * ERROR ROUTES
--- themes/cafecontrol/blog-post.php
+++ themes/cafecontrol/blog-post.php
@@ -119,15 +119,17 @@
 
 
 <?php $v->start("scripts"); ?>
-<div id="fb-root"></div>
-<script>(function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.1&appId=267654637306249&autoLogAppEvents=1';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+    <div id="fb-root"></div>
+    <script>
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v3.1&appId=267654637306249&autoLogAppEvents=1';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 <?php $v->end(); ?>
