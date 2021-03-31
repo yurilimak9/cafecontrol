80672a16e23784c08b077063b1758619418aaf85
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-03-31 12:29:36 -0300

    Aula: Acesso e estatísticas internas

--- source/App/Web.php
+++ source/App/Web.php
@@ -9,6 +9,7 @@ use Source\Models\Auth;
 use Source\Models\Category;
 use Source\Models\Faq\Question;
 use Source\Models\Post;
+use Source\Models\Report\Access;
 use Source\Models\User;
 use Source\Support\Email;
 use Source\Support\Pager;
@@ -25,6 +26,8 @@ class Web extends Controller
     public function __construct()
     {
         parent::__construct(__DIR__ . "/../../themes/" . CONF_VIEW_THEME . "/");
+
+        (new Access())->report();
     }
 
     /**
