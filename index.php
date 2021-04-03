d68cebdd3b57f42731ee38f16fee21a64b649623
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-04-02 23:17:37 -0300

    Aula: Listando receitas e despesas

--- index.php
+++ index.php
@@ -58,13 +58,16 @@ $route->get("/termos", "Web:terms");
 $route->group("/app");
 $route->get("/", "App:home");
 $route->get("/receber", "App:income");
+$route->get("/receber/{status}/{category}/{date}", "App:income");
 $route->get("/pagar", "App:expense");
+$route->get("/pagar/{status}/{category}/{date}", "App:expense");
 $route->get("/fatura/{invoice}", "App:invoice");
 $route->get("/perfil", "App:profile");
 $route->get("/sair", "App:logout");
 
 $route->post("/launch", "App:launch");
 $route->post("/support", "App:support");
+$route->post("/filter", "App:filter");
 
 
 /**
