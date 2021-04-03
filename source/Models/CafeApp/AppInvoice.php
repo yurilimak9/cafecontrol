fe1dc5e29fbe730c38c9242959cd53c0205a453e
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-04-02 23:59:31 -0300

    Aula: Normalizando lançamentos

--- source/Models/CafeApp/AppInvoice.php
+++ source/Models/CafeApp/AppInvoice.php
@@ -27,6 +27,55 @@ class AppInvoice extends Model
 
     /**
      * @param User $user
+     * @param int $afterMonths
+     */
+    public function fixed(User $user, int $afterMonths = 1): void
+    {
+        $fixed = $this->find(
+            "user_id = :user AND status = 'paid' AND type IN('fixed_income', 'fixed_expense')",
+            "user={$user->id}"
+        )->fetch(true);
+
+        if (!$fixed) {
+            return;
+        }
+
+        foreach ($fixed as $fixedItem) {
+            $invoice = $fixedItem->id;
+            $start = new \DateTime($fixedItem->due_at);
+            $end = new \DateTime("+{$afterMonths}month");
+
+            if ($fixedItem->period == "month") {
+                $interval = new \DateInterval("P1M");
+            }
+
+            if ($fixedItem->period == "year") {
+                $interval = new \DateInterval("P1Y");
+            }
+
+            $period = new \DatePeriod($start, $interval, $end);
+            foreach ($period as $item) {
+                $getFixed = $this->find(
+                    "user_id = :user AND invoice_of = :of AND year(due_at) = :y AND month(due_at) = :m",
+                    "user={$user->id}&of={$fixedItem->id}&y={$item->format("Y")}&m={$item->format("m")}",
+                    "id"
+                )->fetch();
+
+                if (!$getFixed) {
+                    $newItem = $fixedItem;
+                    $newItem->id = null;
+                    $newItem->invoice_of = $invoice;
+                    $newItem->type = str_replace("fixed_", "", $newItem->type);
+                    $newItem->due_at = $item->format("Y-m-d");
+                    $newItem->status = ($item->format("Y-m-d") <= date("Y-m-d") ? "paid" : "unpaid");
+                    $newItem->save();
+                }
+            }
+        }
+    }
+
+    /**
+     * @param User $user
      * @param string $type
      * @param array|null $filter
      * @param int|null $limit
