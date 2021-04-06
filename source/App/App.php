a3ada8d0a874e72c537d07bae9f7f6267143f044
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-04-05 22:59:30 -0300

    Aula: Listando contas recorrentes

--- source/App/App.php
+++ source/App/App.php
@@ -249,6 +249,28 @@ class App extends Controller
     }
 
     /**
+     * 
+     */
+    public function fixed(): void
+    {
+        $head = $this->seo->render(
+            "Minhas contas fixas - " . CONF_SITE_NAME,
+            CONF_SITE_DESC,
+            url(),
+            theme("/assets/images/share.jpg"),
+            false
+        );
+
+        echo $this->view->render("recurrences", [
+            "head" => $head,
+            "invoices" => (new AppInvoice())->find(
+                "user_id = :user AND type IN('fixed_income', 'fixed_expense')",
+                "user={$this->user->id}"
+            )->fetch(true)
+        ]);
+    }
+
+    /**
      * @param array $data
      */
     public function launch(array $data): void
