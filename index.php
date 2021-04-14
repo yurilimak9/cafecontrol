8374152bf89edbd141adda97ad7349a60af1eda8
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-04-14 11:10:34 -0300

    Aula: Design, layout e template

--- index.php
+++ index.php
@@ -80,6 +80,77 @@ $route->post("/wallets/{wallet}", "App:wallets");
 
 
 /**
+ * ADMIN ROUTES
+ */
+$route->namespace("Source\App\Admin");
+$route->group("/admin");
+
+//login
+$route->get("/", "Login:root");
+$route->get("/login", "Login:login");
+$route->post("/login", "Login:login");
+
+//dash
+$route->get("/dash", "Dash:dash");
+$route->get("/dash/home", "Dash:home");
+$route->post("/dash/home", "Dash:home");
+$route->get("/logoff", "Dash:logoff");
+
+//control
+$route->get("/control/home", "Control:home");
+$route->get("/control/subscriptions", "Control:subscriptions");
+$route->post("/control/subscriptions", "Control:subscriptions");
+$route->get("/control/subscriptions/{search}/{page}", "Control:subscriptions");
+$route->get("/control/subscription/{id}", "Control:subscription");
+$route->post("/control/subscription/{id}", "Control:subscription");
+$route->get("/control/plans", "Control:plans");
+$route->get("/control/plans/{page}", "Control:plans");
+$route->get("/control/plan", "Control:plan");
+$route->post("/control/plan", "Control:plan");
+$route->get("/control/plan/{plan_id}", "Control:plan");
+$route->post("/control/plan/{plan_id}", "Control:plan");
+
+//blog
+$route->get("/blog/home", "Blog:home");
+$route->post("/blog/home", "Blog:home");
+$route->get("/blog/home/{search}/{page}", "Blog:home");
+$route->get("/blog/post", "Blog:post");
+$route->post("/blog/post", "Blog:post");
+$route->get("/blog/post/{post_id}", "Blog:post");
+$route->post("/blog/post/{post_id}", "Blog:post");
+$route->get("/blog/categories", "Blog:categories");
+$route->get("/blog/categories/{page}", "Blog:categories");
+$route->get("/blog/category", "Blog:category");
+$route->post("/blog/category", "Blog:category");
+$route->get("/blog/category/{category_id}", "Blog:category");
+$route->post("/blog/category/{category_id}", "Blog:category");
+
+//faqs
+$route->get("/faq/home", "Faq:home");
+$route->get("/faq/home/{page}", "Faq:home");
+$route->get("/faq/channel", "Faq:channel");
+$route->post("/faq/channel", "Faq:channel");
+$route->get("/faq/channel/{channel_id}", "Faq:channel");
+$route->post("/faq/channel/{channel_id}", "Faq:channel");
+$route->get("/faq/question/{channel_id}", "Faq:question");
+$route->post("/faq/question/{channel_id}", "Faq:question");
+$route->get("/faq/question/{channel_id}/{question_id}", "Faq:question");
+$route->post("/faq/question/{channel_id}/{question_id}", "Faq:question");
+
+//users
+$route->get("/users/home", "Users:home");
+$route->post("/users/home", "Users:home");
+$route->get("/users/home/{search}/{page}", "Users:home");
+$route->get("/users/user", "Users:user");
+$route->post("/users/user", "Users:user");
+$route->get("/users/user/{user_id}", "Users:user");
+$route->post("/users/user/{user_id}", "Users:user");
+
+//END ADMIN
+$route->namespace("Source\App");
+
+
+/**
  * PAY ROUTES
  */
 $route->group("/pay");
