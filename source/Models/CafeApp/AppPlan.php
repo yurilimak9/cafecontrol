8da4e3e36ce39e5e92c32c4bc169d36224e158b5
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-04-16 12:04:25 -0300

    Aula: Lista e gestão de planos

--- source/Models/CafeApp/AppPlan.php
+++ source/Models/CafeApp/AppPlan.php
@@ -19,4 +19,28 @@ class AppPlan extends Model
     {
         parent::__construct("app_plans", ["id"], ["name", "period", "period_str", "price", "status"]);
     }
+
+    /**
+     * @param string|null $status
+     * @return AppSubscription|null
+     */
+    public function subscribers(?string $status = "active"): ?AppSubscription
+    {
+        if ($status) {
+            return (new AppSubscription())->find(
+                "plan_id = :plan AND pay_status = :status",
+                "plan={$this->id}&status={$status}"
+            );
+        }
+
+        return (new AppSubscription())->find("plan_id = :plan", "plan={$this->id}");
+    }
+
+    /**
+     * @return int
+     */
+    public function recurrence(): int
+    {
+        return ($this->subscribers()->count() * $this->price);
+    }
 }
